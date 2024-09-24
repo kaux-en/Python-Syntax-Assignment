6cc72363ddb90b22691c155a1820f9d85403a953
keeks-co <08araik@gmail.com>
2024-09-24 13:39:40 -0500

    Python Syntax Assignment

---
+++ #Arithmetic Operations in Daily Life.py
@@ -0,0 +1,15 @@
+#Arithmetic Operations in Daily Life
+
+# Task 1: Grocery Store Math
+pb = 3.50
+apples = 2
+napkins = 15.60 
+
+result = pb + apples + napkins 
+print(result)
+
+#Task 2: Bank Interest
+initial_amount = 15000
+interest = 0.08
+result = (15000 * 0.08) + 15000
+print(result) 
---
+++ #Showcase Your Dance Moves.py
@@ -0,0 +1,19 @@
+#Showcase Your Dance Moves
+
+# Task 1: Code Correction
+
+weather = "Sunny"
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+# Task 2: Your Mood Today
+
+mood = input("How do you feel today?")
+
+if mood == "happy":
+    print("That's great to hear!")
+else:
+    print("I hope your day gets better!")
+
